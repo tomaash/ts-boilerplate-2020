b987e739d3bd7df0cd401f0b3dd351ea1b32774c
Tomas Holas <tomas.holas@gmail.com>
2020-05-21 16:36:49 +0200

    add mobx

--- src/components/thing.tsx
+++ src/components/thing.tsx
@@ -1,9 +1,24 @@
-import React, { FC } from "react";
+import React, { FC } from 'react'
+import { observer } from 'mobx-react'
+import { observable } from 'mobx'
 
-export const Thing: FC = (props) => {
-  return (
-    <div>
-      <h1>this is a thing</h1>
-    </div>
-  );
-};
+// export const Thing: FC = (props) => {
+//   return (
+//     <div>
+//       <h1>this is a thing</h1>
+//     </div>
+//   );
+// };
+
+@observer
+export class Thing extends React.Component {
+  @observable cnt = 0
+  render() {
+    return (
+      <div>
+        <h1>this is a thing {this.cnt}</h1>
+        <button onClick={() => this.cnt++}>INC</button>
+      </div>
+    )
+  }
+}
